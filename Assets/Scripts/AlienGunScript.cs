9b46ee5d70f6ab910cbcd3b670001952547661e5
Sam <samuel.levine@gmail.com>
2016-01-29 17:11:57 -0600

    Mission target gibbing!

--- Assets/Scripts/AlienGunScript.cs
+++ Assets/Scripts/AlienGunScript.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class AlienGunScript : MonoBehaviour {
     public int whichHand = 0; // 0 = left, 1 = right
     public ParticleSystem particles;
     public Transform emitPoint;
+    public float raycastDistance = 10.0f;
 
     void Update () {
-
         if (SixenseInput.Controllers[whichHand].Enabled) {
             if (SixenseInput.Controllers[whichHand].GetButtonDown(SixenseButtons.JOYSTICK)) {
                 fireLaser();
@@ -18,5 +19,17 @@ public class AlienGunScript : MonoBehaviour {
     private void fireLaser() {
         Debug.Log("PEW PEW");
         GameObject.Instantiate(particles, emitPoint.position, emitPoint.rotation);
+
+        //Do a RAYCAST!  if you hit the Victim, tell him to explode.
+        RaycastHit hit;
+        Vector3 fwd = emitPoint.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(emitPoint.position, fwd, out hit, raycastDistance)) {
+            print("RAYCAST HIT!  hit = " + hit.collider.name);
+            if (hit.collider.tag == "Victim")
+            {
+                VictimScript v = hit.collider.GetComponent<VictimScript>();
+                v.explode();
+            }
+        }
     }
 }
---
+++ Assets/Scripts/VictimScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictimScript : MonoBehaviour {
+    public GameObject meatPrefab;
+    public int numMeatChunks = 40;
+    public bool stayDead = false;
+
+    public float explosionForce = 40f;
+    public float explosionRadius = 5f;
+
+    public void explode() {
+        // Create a ton of meat prefabs and send them out.
+        for (int i = 0; i < numMeatChunks; i++)
+        {
+            GameObject go = (GameObject) GameObject.Instantiate(meatPrefab, this.transform.position + Random.insideUnitSphere + Vector3.up*2.0f, Random.rotation);
+            go.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, this.transform.position, explosionRadius);
+        }
+
+        Debug.Log("KABOOM");
+        // TODO: Create an explosives prefab.  Detonator?
+        // TODO: Put a ton of blood on the walls by enabling a hidden gameobject.
+        if (stayDead)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
