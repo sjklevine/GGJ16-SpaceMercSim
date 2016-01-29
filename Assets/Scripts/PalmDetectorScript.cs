4fd7f6aa0873b7ff0b454a4cc7fe164c8115de20
Sam <samuel.levine@gmail.com>
2016-01-29 07:49:59 -0600

    Quick and dirty "mission" environment before naptime.

---
+++ Assets/Scripts/PalmDetectorScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PalmDetectorScript : MonoBehaviour {
+    void OnCollisionEnter(Collision collision)
+    {
+        //TODO: SAM HERE; add audio or force or trigger aniamations or do anything cool@
+        Debug.Log("Palm " + this.name + " collided with " + gameObject.name + "!");
+        /*
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        if (collision.relativeVelocity.magnitude > 2)
+            audio.Play();
+       */
+    }
+}
