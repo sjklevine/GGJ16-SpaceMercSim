f22729a85bfefd8821797df4248056073618a36e
Reynaldo Rodriguez <Reyronald19@hotmail.com>
2016-01-31 10:55:42 -0500

    We have created a title screen, and a credit Screen. It has Play button, credit button and back button.

---
+++ Assets/Scripts/TitleScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class TitleScript : MonoBehaviour {
+    public GameObject StartUI;
+    public GameObject CreditsUI;
+    
+
+    public void onPressPlay() { SceneManager.LoadScene("ApartmentStart"); }
+    public void onPressCredits()
+    {
+        StartUI.SetActive(false);
+        CreditsUI.SetActive(true);
+
+
+    }
+    public void onPressBack()
+    {
+        StartUI.SetActive(true);
+        CreditsUI.SetActive(false);
+
+    }
+}
