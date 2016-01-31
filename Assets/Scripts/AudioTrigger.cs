8f9b879ebf1c6bfd97b6a9105ed4e5106d877825
Ernesto Rojo <gamedev@ernestorojo.com>
2016-01-31 15:28:54 -0500

    Simple script to attach to Triggers which play a sfx

---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioTrigger : MonoBehaviour
+{
+    public AudioClip _clip = null;
+    private AudioSource _audioSource = null;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            _audioSource.PlayOneShot(_clip);
+        }
+    }
+}
